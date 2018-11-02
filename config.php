6a5cfd490e7f0c03c0c420aab3481d4a8f37636a
abdvc <34377465+abdvc@users.noreply.github.com>
2018-11-02 22:42:41 +0500

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root2');
+define('DB_USERNAME', 'abd');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'PaintShop');
  
