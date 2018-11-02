76884906f7dbd531c80c9e7b36da8ccddcfceaaf
abdvc <34377465+abdvc@users.noreply.github.com>
2018-11-02 22:32:44 +0500

    Update delete.php

--- Salesorder/delete.php
+++ Salesorder/delete.php
@@ -1,5 +1,5 @@
 <?php  
- $connect = mysqli_connect("localhost", "root", "", "PaintShop");  
+ $connect = mysqli_connect("localhost", "abd", "", "PaintShop");  
  $sql = "DELETE FROM Order_13022 WHERE OrderNo = '".$_POST["id"]."'";  
  if(mysqli_query($connect, $sql))  
  {  
