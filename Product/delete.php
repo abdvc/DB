d9a227aeb6ea858ac97d8ed5a35589bcf340f790
abdvc <34377465+abdvc@users.noreply.github.com>
2018-11-06 01:44:39 +0500

    Add files via upload

--- Product/delete.php
+++ Product/delete.php
@@ -13,10 +13,7 @@ $id = $_GET['id'];
 $sql = "DELETE FROM Product_13022 WHERE PCode = '$id'";
 
 if($mysqli->query($sql) === TRUE) {
-	echo "<div align='center'>";
-	echo "<h3>Record deleted</h3>";
-	echo "<a href='table.php'><button type='button'>Back</button></a>";
-	echo "</div>";
+	header('location: table.php');
 } else { echo "Error"; }
 ?>
 
--- Product/table.php
+++ Product/table.php
@@ -2,7 +2,36 @@
 <head>
 <title>Product</title>
 </head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="../welcome.php" title="Homepage">My Paint shop</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="../Users/table.php">Users<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../Salesperson/table.php">Salespersons</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="../Customer/table.php">Customers</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link " href="../Product/table.php">Product</a>
+                    </li>
+
+		    
+                  </ul></div>
+			<div align='right'><form action='../logout.php' align='right'>
+			<input type="submit" value="Logout">
+		    </form></div>
+                
+              </nav>
 <div>
+<br><br>
 <?php
 // Include config file
 include("../config.php");
@@ -85,21 +114,34 @@ if($_POST){
 	$size = $_POST['Size'];
 	$price = $_POST['Price'];
 
-	$sql = "INSERT INTO Product_13022 (PCode, Brand, Type, Shade, Size, Price) VALUES ('$pid','$brand','$type','$shade','$size','$price')";
 
-	if ($mysqli->query($sql) === TRUE) {
-		echo "New Record added";
-	} else {
-		echo "Error " . $sql . ' ' . $mysqli->connect_error;
-	}
+	if ($pid!='' || $brand!='' || $type!='' || $shade!='' || $size!='' || $price!='') {
+		$sql = "INSERT INTO Product_13022 (PCode, Brand, Type, Shade, Size, Price) VALUES ('$pid','$brand','$type','$shade','$size','$price')";
+
+		if ($mysqli->query($sql) === TRUE) {
+			echo "New Record added";
+		} else {
+			echo "Error " . $sql . ' ' . $mysqli->connect_error;
+		}
 	
-	$mysqli->close();
+		$mysqli->close();
+	}
 }
 
 
 ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</body>
 <style>
 table, th {
 
@@ -125,7 +167,10 @@ th {
 td{
 	background-color: #FFA07A;
 }
-
+navbar {
+	font-family: verdana;
+    height: 100%;
+}
 </style>
 </html>
 
