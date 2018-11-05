8b40d5cb7d364037bf999c2eb240424e582aa4cc
abdvc <34377465+abdvc@users.noreply.github.com>
2018-11-06 01:42:07 +0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,13 @@
 <?php
+   include('config.php');
    session_start();
-   
+   $usr = $_SESSION['Username'];
+   $a = 0;
+   $sql2 = "UPDATE User_13022 SET Active='$a' WHERE Username='$usr'";
+   if ($mysqli->query($sql2) === TRUE) {
+   echo "Successful update";
+   } else { echo $sql2;}
+
    if(session_destroy()) {
       header("Location: login.php");
    }
