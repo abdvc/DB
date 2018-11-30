0e55d4bd20d6df17d981f148852ff3445685af89
abdvc <34377465+abdvc@users.noreply.github.com>
2018-12-01 03:08:36 +0500

    Update session.php

--- session.php
+++ session.php
@@ -3,6 +3,6 @@
    session_start();
    
    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-      header("location:login.php");
+      header("location:/DB/login.php");
    }
 ?>
